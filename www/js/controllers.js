f4c552ede55677ae34f67040bed2f0dfa9b1fffe
papesdiop <papesdiop@gmail.com>
2014-02-22 17:07:09 +0000

    - theme integration with themeroller - jquery ui progressbar integration - Record feature integration with jqm

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,3 +27,119 @@ function PlayerController($scope, $http, Record) {
         });
     }
 }
+
+function RecordController($scope, Data) {
+    $scope.data = Data;
+    $scope.max = 100;
+
+    var recordPrepare = $scope.recordPrepare = function () {
+        $('#prepareRecord').attr("disabled", false);
+        $('#record').unbind();
+        $('#record').html('Start recording');
+        $('#record').bind('touchstart', function (){
+            //$( "#dialog-modal" ).dialog( "close" );
+            recordAudio($scope, Data);
+        });
+        /*$( "#dialog-modal" ).dialog({
+            autoOpen: false,
+            width: 350,
+            modal: true
+        });
+        $( "#prepareRecord" )
+            .button()
+            .click(function() {
+                $( "#dialog-modal" ).dialog( "open" );
+            });*/
+    };
+
+    function stopRecording() {
+        $("#progressbar" ).progressbar( "destroy" );
+        $('#message').html('Audio file successfully created:<br />' + src);
+        clearInterval(recInterval);
+        audioRecording.stopRecord();
+        recordPrepare();
+    }
+
+    var recordAudio =  function($scope, Data) {
+        //$('#prepareRecord').attr("disabled", true);
+        $('#record').unbind();
+        $('#record').html('Stop recording');
+        $('#stop').bind('touchstart', function() {
+            stopRecording();
+        });
+        src = 'touba_voice/'
+        src += ((Data.fileName==null||Data.fileName === 'undefined') ? 'recording_' + Math.round(new Date().getTime()/1000) : Data.fileName)+Data.ext;
+        audioRecording = new Media(src, onSuccess, onError);
+        Data.countdownInt = 3;
+        var startCountdown = setInterval(function() {
+            $('#message').html('Recording will start in ' + Data.countdownInt + ' seconds...');
+            Data.countdownInt = Data.countdownInt -1;
+            if(Data.countdownInt <= 0) {
+                Data.countdownInt = 3;
+                clearInterval(startCountdown);
+                audioRecording.startRecord();
+                var recTime = maxTime;
+                recInterval = setInterval(function() {
+                    recTime = recTime - 1;
+                    var timeRun = maxTime - recTime
+                    $('#message').html(Math.floor(timeRun/60) +':' + timeRun%60);
+                    var progress = 100-((100/maxTime) * recTime);
+                    $( "#progressbar" ).progressbar({
+                        value: progress
+                    });
+                    $( "#progressbar" ).on( "progressbarcomplete", function( event, ui ) {
+                        //alert('Max time reached')
+                    } );
+                    if (recTime <= 0) {
+                        stopRecording();
+                    }
+                }, 1000);
+            }
+        }, 1000);
+    }
+
+    function onSuccess() {
+        $('#message').html('Audio file successfully created:<br />' + src);
+        send(src); //will be removed TODO
+    }
+
+    function send(recordURI) {
+        console.log('RECORD URI : ' + recordURI);
+        $("#message").html("<p>Sending "+recordURI+"</p>");
+        var options = new FileUploadOptions();
+        options.fileKey = "file";
+        options.fileName = Data.fileName + Data.ext; //recordURI.substr(src.lastIndexOf('/')+1);
+        options.mimeType = Data.ext==='.mp3'?"audio/mpeg":"audio/x-wav"; //audio/mpeg    audio/x-wav
+        options.chunkedMode = false;
+        var fileTransfer = new FileTransfer();
+        /*fileTransfer.onprogress = function(progressEvent) {
+         if (progressEvent.lengthComputable) {
+         //loadingStatus.setPercentage(progressEvent.loaded / progressEvent.total);
+         var progress = 100-((100/progressEvent.total) * progressEvent.loaded );
+         $( "#progressbar" ).progressbar({
+         value: progress
+         });
+         } else {
+         //loadingStatus.increment();
+         }
+         };*/
+        fileTransfer.upload(
+            '/sdcard/'+recordURI,
+            SERVER_ADDRESS+'/records', // Remote server for uploading record
+            fileUploaded,
+            onError,
+            options
+        );
+        //$('#message').html('Audio file successfully sent:<br />' + src);
+    }
+
+    function fileUploaded(result) {
+        $("#message").html('<p>Upload complete!!<br />Bytes sent: ' + result.bytesSent + '</p>');
+        //$("#returnMessage").attr("src", result.response);
+    }
+
+    function onError(error) {
+        $('#message').html('code: ' + error.code + 'message: ' + error.message + '\n');
+        console.dir(error)
+    }
+}
--- www/js/services.js
+++ www/js/services.js
@@ -1,8 +1,43 @@
-var SERVER_ADDRESS = 'http://toubavoiceserver-software.rhcloud.com';
+var maxTime = 1000, // Max time for record in seconde
+    src,
+    audioRecording,
+    stopRecording,
+    recInterval,
+    directory,
+    reader,
+    SERVER_ADDRESS = 'http://toubavoiceserver-software.rhcloud.com';
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+/** Records directory creation **/
+function onRequestFileSystemSuccess(fileSystem) {
+    var entry=fileSystem.root;
+    entry.getDirectory("touba_voice/", {create: true, exclusive: false},
+        function (dir) {
+            console.log("Created dir "+ dir.name);
+            directory = dir;
+            reader = dir.createReader();
+        }
+        ,function (error) {
+            //alert("Error creating directory" + error.code);
+        });
+}
+
+function onDeviceReady($scope, Data) {
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestFileSystemSuccess, null);
+}
 
 var module1 = angular.module('RecordService', ['ngResource']).factory('Record', ['$resource', function ($resource) {
     var Record = $resource(SERVER_ADDRESS+'/records', {}, {
         update: { method: 'PUT'}
     });
     return Record;
-}]);
+    }])
+    .factory('Data', function(){
+        return {
+            fileName: 'My Recording',
+            ext: '.mp3', //file extension .mp3, .wav
+            maxTime : 10, // Max time for record in seconde
+            src:null
+        }
+    })
