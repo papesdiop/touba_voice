d15aabf031ef7650344b9865c009ab2598b49133
papesdiop <papesdiop@gmail.com>
2014-03-06 16:42:04 +0000

    cordova application name updated

--- platforms/android/bin/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/papesdiop/C4A/DEVELOP/toubavoice/platforms/android/bin/HelloCordova-debug-unaligned.apk : \
-/home/papesdiop/C4A/DEVELOP/toubavoice/platforms/android/bin/HelloCordova.ap_ \
-/home/papesdiop/C4A/DEVELOP/toubavoice/platforms/android/bin/classes.dex \
-/home/papesdiop/C4A/DEVELOP/toubavoice/platforms/android/CordovaLib/bin/classes.jar \
